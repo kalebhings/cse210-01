017bc0da1be85f6762ab5bf18e3e32d412acd072
kalebhings <kalebhings@gmail.com>
2022-05-21 16:13:04 -0600

    Wrote 6 more methods and finished the game

--- Program.cs
+++ Program.cs
@@ -26,8 +26,139 @@ namespace cse210_01
                 /* creates board */
                 displayBoard(boardSlot);
 
+                /* Player chooses where to make move */
+                makeMove(boardSlot, playerTurn);
+
+                /* Check if there is a win or a draw */
+                game = checkWinner(boardSlot);
 
            } while (game.Equals(0));
+        }
+
+        private static int checkWinner(char[] boardSlot)
+        {
+            if (hasWinner(boardSlot))
+            {
+                return 1;
+            }
+
+            if (isDraw(boardSlot))
+            {
+                return 2;
+            }
+
+            return 0;
+        }
+        private static bool isDraw(char[] boardSlot)
+        {   
+            /* Check if all game markers are replaced by "O" or "X" */
+            return boardSlot[0] != '1' &&
+                   boardSlot[1] != '2' &&
+                   boardSlot[2] != '3' &&
+                   boardSlot[3] != '4' &&
+                   boardSlot[4] != '5' &&
+                   boardSlot[5] != '6' &&
+                   boardSlot[6] != '7' &&
+                   boardSlot[7] != '8' &&
+                   boardSlot[8] != '9';
+        }
+        private static bool hasWinner(char[] boardSlot)
+        {   
+            /* Checks to see if there is a winner*/
+             if (IsSlotPlacementTheSame(boardSlot, 0, 1, 2))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 3, 4, 5))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 6, 7, 8))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 0, 3, 6))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 1, 4, 7))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 2, 5, 8))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 0, 4, 8))
+            {
+                return true;
+            }
+
+            if (IsSlotPlacementTheSame(boardSlot, 2, 4, 6))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsSlotPlacementTheSame(char[] testSlotPlacement, int pos1, int pos2, int pos3)
+        {
+            return testSlotPlacement[pos1].Equals(testSlotPlacement[pos2]) && testSlotPlacement[pos2].Equals(testSlotPlacement[pos3]);
+        }
+
+        private static void makeMove(char[] boardSlot, int currentPlayer)
+        {   
+            bool notValidMove = true;
+
+            do
+            {
+                string userInput = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(userInput) &&
+                    (userInput.Equals("1") ||
+                    userInput.Equals("2") ||
+                    userInput.Equals("3") ||
+                    userInput.Equals("4") ||
+                    userInput.Equals("5") ||
+                    userInput.Equals("6") ||
+                    userInput.Equals("7") ||
+                    userInput.Equals("8") ||
+                    userInput.Equals("9")))
+                {
+
+                    int.TryParse(userInput, out var slotPlacement);
+
+                    char currentTurn = boardSlot[slotPlacement - 1];
+
+                    if (currentTurn.Equals('X') || currentTurn.Equals('O'))
+                    {
+                        Console.WriteLine("Selection already taken, please try a new spot.");
+                    }
+                    else
+                    {
+                        boardSlot[slotPlacement - 1] = nextPlayer(currentPlayer);
+
+                        notValidMove = false;
+                    }
+                }
+            } while (notValidMove);
+        }
+
+        private static char nextPlayer(int player)
+        {
+            if(player % 2 == 0)
+            {
+                return 'O';
+            }
+
+            return 'X';
         }
 
         static void startGame(int playerNumber)
